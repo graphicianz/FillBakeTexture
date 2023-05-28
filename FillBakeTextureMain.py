4ea260d44bc8adca754f9791683cf4488e67f646
chaiwit jarunyakorn <chaiwit.3d@gmail.com>
2023-05-28 17:15:03 +0700

    wip2
    create empty canvas that resolution the same as input .png image. And while read each input pixel, if pixel has A channel less than 255, put pixel color (255,0,0) in empty canvas with the same x,y coordinate.

--- FillBakeTextureMain.py
+++ FillBakeTextureMain.py
@@ -1,21 +1,29 @@
 from PIL import Image
 
 # Open the image file
-image = Image.open("images/test01.png")
+input_image = Image.open("images/test01.png")
 
-# Convert the image to RGB mode (if it's not already)
-image = image.convert("RGB")
+# Convert the image to RGBA mode (if it's not already)
+input_image = input_image.convert("RGBA")
 
 # Get the width and height of the image
-width, height = image.size
+width, height = input_image.size
 
-# Iterate over each pixel in the image
+# Create an empty canvas with the same resolution
+output_image = Image.new("RGBA", (width, height))
+
+# Iterate over each pixel in the input image
 for y in range(height):
     for x in range(width):
-        # Get the RGB color values of the pixel at (x, y)
-        r, g, b = image.getpixel((x, y))
+        # Get the RGBA color values of the pixel at (x, y)
+        r, g, b, a = input_image.getpixel((x, y))
 
-        # Do something with the color values
-        # For example, print them
-        print(f"Pixel at ({x}, {y}) - R: {r}, G: {g}, B: {b}")
-    break
+        # Check if the alpha channel is less than 255
+        if a < 255:
+            # Set the pixel color to red on the output canvas
+            output_image.putpixel((x, y), (255, 0, 0, 255))
+
+
+
+# Save the output image
+output_image.save("images/test01_output.png")
