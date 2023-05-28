f63eb5318a01b6593a3620b5570cf4350c415594
chaiwit jarunyakorn <chaiwit.3d@gmail.com>
2023-05-28 16:47:21 +0700

    wip01

--- FillBakeTextureMain.py
+++ FillBakeTextureMain.py
@@ -1,1 +1,21 @@
-pass
+from PIL import Image
+
+# Open the image file
+image = Image.open("images/test01.png")
+
+# Convert the image to RGB mode (if it's not already)
+image = image.convert("RGB")
+
+# Get the width and height of the image
+width, height = image.size
+
+# Iterate over each pixel in the image
+for y in range(height):
+    for x in range(width):
+        # Get the RGB color values of the pixel at (x, y)
+        r, g, b = image.getpixel((x, y))
+
+        # Do something with the color values
+        # For example, print them
+        print(f"Pixel at ({x}, {y}) - R: {r}, G: {g}, B: {b}")
+    break
